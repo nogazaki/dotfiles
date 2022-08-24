33a15fd3e3e2d6d50a672aabbfaf96f8b1181bb0
nogazaki <tarutojido@gmail.com>
2022-08-24 21:23:34 +0700

    fix (partially) wibox.widget.slider
    check awesomewm issue-3042 for details

--- .config/awesome/capi.lua
+++ .config/awesome/capi.lua
@@ -1,8 +1,11 @@
+---@diagnostic disable: undefined-field
+
 return {
-    awesome = _G.awesome,
-    root    = _G.root,
-    mouse   = _G.mouse,
-    screen  = _G.screen,
-    tag     = _G.tag,
-    client  = _G.client,
+    awesome      = _G.awesome,
+    root         = _G.root,
+    mouse        = _G.mouse,
+    mousegrabber = _G.mousegrabber,
+    screen       = _G.screen,
+    tag          = _G.tag,
+    client       = _G.client,
 }
